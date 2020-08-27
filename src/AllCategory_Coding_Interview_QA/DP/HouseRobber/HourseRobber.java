1d2c23f90c12a03858bd74e845a9b142d2e6a6d3
eric-programming <eric09279@outlook.com>
2020-08-27 15:08:24 -0700

    updates

--- src/AllCategory_Coding_Interview_QA/DP/HouseRobber/HourseRobber.java
+++ src/AllCategory_Coding_Interview_QA/DP/HouseRobber/HourseRobber.java
@@ -2,13 +2,13 @@ package AllCategory_Coding_Interview_QA.DP.HouseRobber;
 
 public class HourseRobber {
     public static void main(String[] args) {
-        System.out.println(rob(new int[] { 5, 2, 3, 10, 1 }));
-        System.out.println(rob(new int[] { 2, 1, 1, 1, 10, 100 }));
-        System.out.println(rob(new int[] {}));
-        System.out.println(rob(new int[] { 1 }));
-        System.out.println(rob(new int[] { 1, 2 }));
-        System.out.println(rob(new int[] { 2, 1 }));
-        System.out.println(rob(new int[] { 2, 1, 1, 2 }));
+        System.out.println(rob(new int[] { 5, 2, 3, 10, 1 }));// 15
+        System.out.println(rob(new int[] { 2, 1, 1, 1, 10, 100 }));// 103
+        System.out.println(rob(new int[] {}));// 0
+        System.out.println(rob(new int[] { 1 }));// 1
+        System.out.println(rob(new int[] { 1, 2 }));// 2
+        System.out.println(rob(new int[] { 2, 1 }));// 2
+        System.out.println(rob(new int[] { 2, 1, 1, 2 }));// 4
     }
 
     /**
@@ -16,22 +16,22 @@ public class HourseRobber {
      * the key formula
      */
     public static int rob(int[] nums) {
-        // nums.length == 0
+        // if length == 0
         if (nums.length == 0) {
             return 0;
         }
-        // nums.length==1
+        // if length == 1
         if (nums.length == 1) {
             return nums[0];
         }
-        // nums.length==2
-        if (nums.length == 1) {
+        // if length == 2
+        if (nums.length == 2) {
             return Math.max(nums[0], nums[1]);
         }
-        // nums.length > 2
-        int p1 = nums[0], p2 = 0;
+        // else find the max amount that we can take
+        int p2 = 0, p1 = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            int cur = Math.max(p2 + nums[i], p1);
+            int cur = Math.max(p1, p2 + nums[i]);
             p2 = p1;
             p1 = cur;
         }
