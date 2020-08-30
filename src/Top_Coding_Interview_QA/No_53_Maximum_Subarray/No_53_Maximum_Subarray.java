e13915dab3137cddddfef72124951bcb6cac60b3
eric-programming <eric09279@outlook.com>
2020-08-29 20:41:23 -0700

    updates

--- src/Top_Coding_Interview_QA/No_53_Maximum_Subarray/No_53_Maximum_Subarray.java
+++ src/Top_Coding_Interview_QA/No_53_Maximum_Subarray/No_53_Maximum_Subarray.java
@@ -7,7 +7,6 @@ public class No_53_Maximum_Subarray {
     public static void main(String[] args) {
         System.out.println(maxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));// 6
         System.out.println(maxSubArray(new int[] {}));// 0
-        System.out.println(maxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));// 6
         System.out.println(maxSubArray(new int[] { -2, 1 }));// 1
         System.out.println(maxSubArray(new int[] { -1, 1, 2, 1 }));// 4
     }
