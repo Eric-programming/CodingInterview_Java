14385e72910e2410ddddb42176998a5061d9e6b8
eric-programming <eric09279@outlook.com>
2020-11-14 15:54:08 -0800

    update p6

--- Foundation/Recursion/p6_509_Fibonacci_Number/p6_509_Fibonacci_Number.java
+++ Foundation/Recursion/p6_509_Fibonacci_Number/p6_509_Fibonacci_Number.java
@@ -9,30 +9,38 @@ public class p6_509_Fibonacci_Number {
         System.out.println(fib(4));// 3
     }
 
-    public static int fib(int N) {
-        if (N <= 1) {
-            return N;
-        }
-        return fib(N - 1) + fib(N - 2);
-    }
-
+    /**
+     * 
+     * Time Complexity = 2^n
+     */
     // public static int fib(int N) {
     // if (N <= 1) {
     // return N;
     // }
-    // int[] cache = new int[N + 1];
-    // Arrays.fill(cache, -1);
-    // return fib_recur(N, cache);
+    // return fib(N - 1) + fib(N - 2);
     // }
 
-    // public static int fib_recur(int N, int[] cache) {
-    // if (N <= 1) {
-    // return N;
-    // }
-    // if (cache[N] != -1) {
-    // return cache[N];
-    // }
-    // cache[N] = fib_recur(N - 1, cache) + fib_recur(N - 2, cache);
-    // return cache[N];
-    // }
+    public static int fib(int N) {
+        if (N <= 1) {
+            return N;
+        }
+        int[] cache = new int[N + 1];
+        Arrays.fill(cache, -1);
+        return fib_recur(N, cache);
+    }
+
+    /**
+     * 
+     * Time Complexity O(n)
+     */
+    public static int fib_recur(int N, int[] cache) {
+        if (N <= 1) {
+            return N;
+        }
+        if (cache[N] != -1) {
+            return cache[N];
+        }
+        cache[N] = fib_recur(N - 1, cache) + fib_recur(N - 2, cache);
+        return cache[N];
+    }
 }
